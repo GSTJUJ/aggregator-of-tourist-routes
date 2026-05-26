b537bf7f8ff9b9beb299b571108fabd2da286934
GSTJUJ <den_bykov_2001@mail.ru>
2026-05-26 18:21:33 +0700

    connected the frontend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from app.routes import auth
 from app.routes import booking
@@ -6,11 +9,18 @@ from app.routes import tours
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
 app.include_router(auth.router)
 app.include_router(booking.router)
 app.include_router(tours.router)
 
 
-@app.get("/")
-def home():
-    return {"message": "Сервер работает!"}
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="first_page.html"
+    )
---
+++ app/utils/dependencies.py
@@ -0,0 +1,63 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy import text
+
+from app.database import SessionLocal
+from app.config import SECRET_KEY, ALGORITHM
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/login"
+)
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Не удалось проверить токен",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise credentials_exception
+
+    except JWTError:
+        raise credentials_exception
+
+    db = SessionLocal()
+
+    result = db.execute(
+        text(
+            "SELECT * FROM users WHERE user_id = :user_id"
+        ),
+        {"user_id": int(user_id)}
+    ).fetchone()
+
+    db.close()
+
+    if result is None:
+        raise credentials_exception
+
+    return result
+    
