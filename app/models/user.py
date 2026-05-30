b523be1663107608a3ad76955877fd531400fb98
Ivankin-AnV <toni.ivankin.04@mail.ru>
2026-05-30 17:17:12 +0500

    make user-Profile

--- app/models/user.py
+++ app/models/user.py
@@ -10,11 +10,15 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
 
-    username = Column(String(50), unique=True, nullable=False)
+    full_name = Column(String(255), nullable=False)
+
+    phone = Column(String(50), nullable=False)
 
     email = Column(String(255), unique=True, nullable=False)
 
-    hashed_password = Column(String, nullable=False)
+    region = Column(String(255), nullable=False)
+
+    password = Column(String, nullable=False)
 
     created_at = Column(
         DateTime,
